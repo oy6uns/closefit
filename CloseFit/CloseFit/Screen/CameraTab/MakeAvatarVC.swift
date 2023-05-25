563fc9f30f51840009d1e0e0bee935e27c9a80ce
oy6uns <bacho4235@gmail.com>
2023-05-26 01:09:35 +0900

    [Add] Assets 추가

---
+++ CloseFit/CloseFit/Screen/CameraTab/MakeAvatarVC.swift
@@ -0,0 +1,8 @@
+//
+//  MakeAvatarVC.swift
+//  CloseFit
+//
+//  Created by saint on 2023/05/25.
+//
+
+import Foundation
---
+++ CloseFit/CloseFit/Screen/CameraTab/MakeClothVC.swift
@@ -0,0 +1,8 @@
+//
+//  MakeClothVC.swift
+//  CloseFit
+//
+//  Created by saint on 2023/05/25.
+//
+
+import Foundation
