172090242ca0c6aefdad4df7a9e8ebec8bb90099
oy6uns <bacho4235@gmail.com>
2023-05-26 01:09:59 +0900

    [Feat] MakeAvatarVC 구현

--- CloseFit/CloseFit/Screen/CameraTab/MakeAvatarVC.swift
+++ CloseFit/CloseFit/Screen/CameraTab/MakeAvatarVC.swift
@@ -5,4 +5,40 @@
 //  Created by saint on 2023/05/25.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+import Then
+
+class MakeAvatarVC: UIViewController {
+    
+    private let closeBtn = UIButton().then {
+        $0.setBackgroundImage(UIImage(named: "closeBtn"), for: .normal)
+        $0.contentMode = .scaleAspectFit
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setLayout()
+        pressBtn()
+    }
+    
+    private func pressBtn(){
+        closeBtn.press { [self] in
+            self.dismiss(animated: true)
+        }
+    }
+}
+
+extension MakeAvatarVC{
+    private func setLayout(){
+        view.backgroundColor = .systemPurple
+        view.addSubviews([closeBtn])
+        
+        closeBtn.snp.makeConstraints{
+            $0.top.equalTo(self.view.safeAreaLayoutGuide).offset(20)
+            $0.leading.equalTo(self.view.safeAreaLayoutGuide).offset(16)
+            $0.height.width.equalTo(24.adjustedW)
+        }
+    }
+}
+
