d69fb3ec4b498720d125a2cd09dff9892633be0c
oy6uns <bacho4235@gmail.com>
2023-06-04 23:27:31 +0900

    [Feat] Avatartab 동영상 추가

--- CloseFit/CloseFit/Screen/AvatarTab/AvatarVC.swift
+++ CloseFit/CloseFit/Screen/AvatarTab/AvatarVC.swift
@@ -6,25 +6,56 @@
 //
 
 import UIKit
+import SnapKit
+import Then
+import AVKit
 
 class AvatarVC: UIViewController {
+
+    private var player: AVPlayer?
+    
+    private let topView = UIView().then{
+        $0.backgroundColor = .white
+    }
+    
+    private let titleLabel = UILabel().then{
+        $0.text = "나의 아바타"
+        $0.textColor = .black
+        $0.font = .systemFont(ofSize: 20.adjustedW, weight: .medium)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .white
-        // Do any additional setup after loading the view.
+
+        // 영상 파일 경로
+        guard let videoPath = Bundle.main.path(forResource: "avatartab", ofType: "mp4") else {
+            return
+        }
+
+        let videoURL = URL(fileURLWithPath: videoPath)
+        let playerItem = AVPlayerItem(url: videoURL)
+        player = AVPlayer(playerItem: playerItem)
+
+        let playerLayer = AVPlayerLayer(player: player)
+        playerLayer.frame = view.bounds
+        playerLayer.videoGravity = .resizeAspectFill
+        view.layer.addSublayer(playerLayer)
+        
+        view.addSubviews([topView, titleLabel])
+        topView.snp.makeConstraints{
+            $0.top.leading.trailing.equalToSuperview()
+            $0.height.equalTo(140)
+        }
+        
+        titleLabel.snp.makeConstraints{
+            $0.top.equalTo(self.view.safeAreaLayoutGuide).offset(20)
+            $0.centerX.equalToSuperview()
+        }
+
+        // 영상 재생
+        player?.play()
+        navigationController?.navigationBar.isHidden = true
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
